767fee84b829b80feaa93297208b615619a0aab2
Debrup Mondal <debrup.official.365@gmail.com>
2021-09-02 11:13:18 +0530

    currency-exchange-service-initialized

---
+++ src/main/java/com/debrup/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.debrup.currencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+    }
+
+}
