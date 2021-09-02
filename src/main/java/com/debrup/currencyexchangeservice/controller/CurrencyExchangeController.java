403c1a89b537583c1a94f376ec468935173e447a
Debrup Mondal <debrup.official.365@gmail.com>
2021-09-02 11:43:13 +0530

    environment-autowired

--- src/main/java/com/debrup/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/debrup/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.debrup.currencyexchangeservice.controller;
 
 import com.debrup.currencyexchangeservice.entity.CurrencyExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,15 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
                                                   @PathVariable String to){
         CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
-        currencyExchange.setEnvironment("8080");
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
 
         return currencyExchange;
 
