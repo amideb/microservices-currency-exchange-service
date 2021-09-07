89d7201183d869592333e0be2fe1a1fbce5d2bae
Debrup Mondal <debrup.official.365@gmail.com>
2021-09-07 10:14:21 +0530

    controller-updated

--- src/main/java/com/debrup/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/debrup/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -22,7 +22,12 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
                                                   @PathVariable String to){
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+      //  CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+
+        CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+        if(currencyExchange==null){
+            throw new RuntimeException("Unable to find data for " + from + " to " + to);
+        }
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
 
