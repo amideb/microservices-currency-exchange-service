ac493338853897d6f4be2d8a20c3b9413c7352ec
Debrup Mondal <debrup.official.365@gmail.com>
2021-09-07 10:06:56 +0530

    currencyexchangerepo added extended by jpa

---
+++ src/main/java/com/debrup/currencyexchangeservice/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,9 @@
+package com.debrup.currencyexchangeservice.repository;
+
+import com.debrup.currencyexchangeservice.entity.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+
+}
